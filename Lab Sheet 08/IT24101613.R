49a750acd4925e33e66963bda2f9063be6822706
IT24101613 <IT24101613@my.sliit.lk>
2025-09-25 11:17:05 +0530

    added lab sheet 08

---
+++ Lab Sheet 08/IT24101613.R
@@ -0,0 +1,38 @@
+setwd("//Users//sawanyakodagoda//Desktop//IT24101613")
+data<- read.table("Exercise - LaptopsWeights.txt", header=TRUE)
+fix(data)
+attach(data)
+
+names(data)<-c("Weight")
+attach(data)
+
+popmn<-mean(Weight)
+popmn
+pop_sd<-sd(Weight)
+pop_sd
+
+samples<-c()
+n<-c()
+
+for (i in 1:25){
+  s<-sample(Weight,6,replace=TRUE)
+  samples<-cbind(samples,s)
+  n<-c(n,paste('S',i))
+}
+
+colnames(samples)=n
+
+s.means<-apply(samples,2,mean)
+s.means
+s.sds<-apply(samples,2,sd)
+s.sds
+
+samplemean<-mean(s.means)
+samplesd<-sd(s.means)
+
+popmn
+samplemean
+
+true_sd<-pop_sd/sqrt(6)
+true_sd
+samplesd
